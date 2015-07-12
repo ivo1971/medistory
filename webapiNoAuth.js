cd90aead5bd753f05ae74120f7654772b9d13d5a
U-THPPC-ILU\user <user@THPPC-ILU.benelux.tokheim.com>
2015-07-12 12:13:55 +0200

    undo changes

--- webapiNoAuth.js
+++ webapiNoAuth.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 // check that all requests are JSON requests
-//
 router.use(function (req, res, next) {
   console.log('Time: ', Date.now());
   next();
