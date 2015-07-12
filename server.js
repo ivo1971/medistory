70b6286484f3490ba432dce30cffd8de5cf5ff4d
U-THPPC-ILU\user <user@THPPC-ILU.benelux.tokheim.com>
2015-07-12 12:08:47 +0200

    test

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-var express = require('express');
+//requires
+var express      = require('express');
+var webapiNoAuth = require('./webapiNoAuth');
+
+//create global app instance
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+//configure the application port: use the Heroku port when available
+//                                for local tests: use a fixed number 
+app.set('port', (process.env.PORT || 8080));
 
+//handle static file request: server them from the 'static' directory
 app.use(express.static(__dirname + '/static'));
 
-app.get('/', function (req, res) {
-  res.redirect('/index.html');
-});
+//add a route for the website root: redirect it to the index page,
+//                                  which is the basis of the SPA
+app.route('/')
+	.get(function (req, res) {
+		res.redirect('/index.html');
+	});
 
+//add a route for the unauthenticated web API
+app.use('/apiNoAuth', webapiNoAuth);
+
+//run the server
 var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('App listening at http://%s:%s', host, port);
 });
