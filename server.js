b83f091d41f14bbe704e9a1a3559e44adc14acc5
ivo1971 <ivo.luyckx@edpnet.be>
2015-07-12 10:48:06 +0200

    b

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
