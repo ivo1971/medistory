914dc3e638cffe1930f72f5d2ad1707aedb8476d
ivo1971 <ivo.luyckx@edpnet.be>
2015-07-12 10:37:57 +0200

    checked locally

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
